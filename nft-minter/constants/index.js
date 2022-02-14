1736be551f087c52bc5e9507c59bc0c844f3e652
iamverg <dev01.dash@gmail.com>
2022-02-14 21:57:22 +0100

    redeploy

--- nft-minter/constants/index.js
+++ nft-minter/constants/index.js
@@ -581,4 +581,4 @@ export const abi = [
     "type": "receive"
   }
 ];
-export const NFT_CONTRACT_ADDRESS = "0x948B4af52474ACe4bF4B8CBdA488da5BA3a6BcF6";
+export const NFT_CONTRACT_ADDRESS = "0x19712B2613f3c9793f028755f564e365F0b45339";
