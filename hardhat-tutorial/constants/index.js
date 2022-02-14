8bbcd7092e082aa02c5aa126816447e164c94ae7
iamverg <dev01.dash@gmail.com>
2022-02-14 21:43:20 +0100

    metadata update and contract redeploy

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x3CD467b79C9E07380A76B43e3B80182699FEB126";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-n0uli21nl-iamverg.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
